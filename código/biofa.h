5e54fc4a4f9b2c72fdb172b5610bf4d15608004d
Vicente-Ferrerira <diegofrra083@gmail.com>
2024-12-11 10:50:24 -0300

    Update and rename boss.c to biofa.h

--- código/biofa.h
+++ código/biofa.h
@@ -8,7 +8,7 @@ typedef struct {
     int defesa;
 }Boss;
 
-// Função para criar o Boss
+// FunÃ§Ã£o para criar o Boss
 Boss criarBoss() {
     Boss boss;
     strcpy(boss.nome, " Biofa ");
@@ -17,13 +17,13 @@ Boss criarBoss() {
     //boss.defesa = ; 
     return boss;
 }
-// Função para imprimir o status do Boss
+// FunÃ§Ã£o para imprimir o status do Boss
 void imprimirBoss(Boss *boss) {
     printf("\n** Um poderoso %s te desafiou! **\n", boss->nome);
     printf("\n------------------------------STATUS DO BOSS---------------------------------\n");
     printf("%s\n", boss->nome);
     printf("Vida: %d\n", boss->vida);
-    printf("Força: %d\n", boss->forca);
+    printf("ForÃ§a: %d\n", boss->forca);
     //printf("Defesa: %d\n\n", boss->defesa);
 }
 
